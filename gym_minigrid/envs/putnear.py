8ebcf2840da5ebf728e083ed293b92fb38b9cebc
Naziah <naziah28@gmail.com>
2020-10-28 21:13:53 +1000

    logging length not all digblocks

--- gym_minigrid/envs/putnear.py
+++ gym_minigrid/envs/putnear.py
@@ -189,8 +189,8 @@ class PutNearEnv(MiniGridEnv):
             if self.grid.get(ox, oy) is preCarrying:
                 if abs(ox - tx) <= 1 and abs(oy - ty) <= 1:
                     reward += 20 * ((self.numObjs-len(self.selected_blocks)))# self._reward()
-                    logger.info('{}: \t dropped block! {} {}'.format(self.step_count, (self.numObjs-len(self.selected_blocks)), (ox,oy)))
-                    logger.info(f'{self.step_count}: \t {self.selected_blocks} remaining')
+                    logger.info('{}: dropped block! {} {}'.format(self.step_count, (self.numObjs-len(self.selected_blocks)), (ox,oy)))
+                    logger.info(f'{self.step_count}: \t {len(self.selected_blocks)} remaining')
                     self.dropped_block = step_count
                     self.currently_holding = False
 
